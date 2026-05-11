5f8358b63057f20d5371483d397f29fd2aaa2867
PrimeRise <6327970@gmail.com>
2026-05-11 15:05:09 +0300

    Update Regions.kt

---
+++ app/src/main/java/com/ospeo/meshok/Regions.kt
@@ -0,0 +1,27 @@
+package com.ospeo.meshok
+
+val regions = mapOf(
+    "Москва" to "RU/MSK", "СПб" to "RU/SPB", "Краснодар" to "RU/KRD",
+    "Новосибирск" to "RU/NSK", "Екатеринбург" to "RU/EKB", "Владивосток" to "RU/VLD",
+    "Калининград" to "RU/KLD", "Ростов-на-Дону" to "RU/RND", "Казань" to "RU/KZN",
+    "Нижний Новгород" to "RU/NNV", "Воронеж" to "RU/VRN", "Волгоград" to "RU/VLG",
+    "Пермь" to "RU/PRM", "Уфа" to "RU/UFA", "Омск" to "RU/OMS",
+    "Челябинск" to "RU/CHL", "Саратов" to "RU/SAR", "Тюмень" to "RU/TYM",
+    "Красноярск" to "RU/KRY", "Иркутск" to "RU/IRK", "Хабаровск" to "RU/KHB",
+    "Ярославль" to "RU/YAR", "Томск" to "RU/TOM", "Оренбург" to "RU/ORB",
+    "Кемерово" to "RU/KEM", "Рязань" to "RU/RZN", "Пенза" to "RU/PNZ",
+    "Липецк" to "RU/LIP", "Тула" to "RU/TUL", "Курск" to "RU/KUR",
+    "Ставрополь" to "RU/STV", "Сочи" to "RU/SCH", "Тверь" to "RU/TVE",
+    "Ижевск" to "RU/IZH", "Барнаул" to "RU/BRN", "Ульяновск" to "RU/ULY",
+    "Владимир" to "RU/VLA", "Мурманск" to "RU/MUR", "Астрахань" to "RU/AST",
+    "Киров" to "RU/KIR", "Брянск" to "RU/BRY", "Белгород" to "RU/BEL",
+    "Смоленск" to "RU/SMO", "Калуга" to "RU/KLG", "Псков" to "RU/PSK",
+    "Самара" to "RU/SAM", "Севастополь" to "RU/SVS", "Симферополь" to "RU/SMF",
+    "Махачкала" to "RU/MKH", "Грозный" to "RU/GRZ", "Нальчик" to "RU/NAL",
+    "Владикавказ" to "RU/VKZ", "Черкесск" to "RU/CHE", "Йошкар-Ола" to "RU/YSH",
+    "Саранск" to "RU/SRN", "Чебоксары" to "RU/CHB", "Тамбов" to "RU/TMB",
+    "Кострома" to "RU/KST", "Вологда" to "RU/VGD", "Архангельск" to "RU/ARH",
+    "Сыктывкар" to "RU/STK", "Якутск" to "RU/YKT", "Магадан" to "RU/MGD",
+    "Южно-Сахалинск" to "RU/USS", "Чита" to "RU/CHI", "Улан-Удэ" to "RU/UUD",
+    "Вся Россия" to "RU/+", "Весь мир" to "+/+", "Европа" to "EU_868", "США" to "US", "Своё..." to "__custom__"
+)
